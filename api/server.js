db46fb6316d35d9d8deec5637f5f1e4846832111
Robert Gripshover <robertgrips@gmail.com>
2023-03-07 10:44:22 -0500

    made some changes to the server file

--- api/server.js
+++ api/server.js
@@ -6,5 +6,10 @@ const server = express() //<< this is the instance of the express app
 
 server.use(express.json()) //<< gives express the ability to read and parse JSON
 
+server.get('/hello-world', (req, res) => {
+    res.status(200).json({ message: "hello, world" })
+})
+
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
+////////
--- index.js
+++ index.js
@@ -6,5 +6,7 @@ server.listen(port, () => {
     console.log(`server started on http://localhost:9000`)
 })
 
+console.log('hey you!')
+
 
 // START YOUR SERVER HERE
