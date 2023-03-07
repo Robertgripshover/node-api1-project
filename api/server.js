120f9ec4cc1b8abbfe9b95012dcbcedb04f781e2
Robert Gripshover <robertgrips@gmail.com>
2023-03-07 11:14:44 -0500

    made more changes to the findById protion of my server.js

--- api/server.js
+++ api/server.js
@@ -2,6 +2,8 @@
 
 const express = require('express') //<< importing express into the project
 
+const User = require('./users/model')
+
 const server = express() //<< this is the instance of the express app
 
 server.use(express.json()) //<< gives express the ability to read and parse JSON
@@ -9,8 +11,35 @@ server.use(express.json()) //<< gives express the ability to read and parse JSON
 
 
 server.get('/api/users', (req, res) => {
-    res.json('users')
+    User.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "error getting users",
+                err: err.message
+            })
+        })
 })
+
+
+
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            res.json(user)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "error getting users",
+                err: err.message
+            })
+        })
+})
+
+
 
 
 server.use('*', (req, res) => {
