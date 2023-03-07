b53a0d9b202b935ff9c01c041c720e531fcbee2b
Robert Gripshover <robertgrips@gmail.com>
2023-03-07 10:04:08 -0500

    added express to the project and I am starting to work on the server.js and index.js files

--- api/server.js
+++ api/server.js
@@ -1,3 +1,10 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require('express') //<< importing express into the project
+
+const server = express() //<< this is the instance of the express app
+
+server.use(express.json()) //<< gives express the ability to read and parse JSON
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -2,4 +2,9 @@ const server = require('./api/server');
 
 const port = 9000;
 
+server.listen(port, () => {
+    console.log(`server started on http://localhost:9000`)
+})
+
+
 // START YOUR SERVER HERE
