8d1fb60b8b066403f7b27d730583c765cac7d800
Robert Gripshover <robertgrips@gmail.com>
2023-03-08 13:24:15 -0500

    I was able to get the put to work

--- api/server.js
+++ api/server.js
@@ -20,6 +20,12 @@ server.put('/api/users/:id', async (req, res) => {
                 res.status(400).json({
                     message: 'Please provide name and bio for the user' 
                 })
+            } else {
+               const updatedUser = await User.update(
+                    req.params.id,
+                    req.body
+               )
+                res.status(200).json(updatedUser)
             }
         }
     } catch (err) {
