8215782d9f0dcb51cf7996d26aede18953171f1b
Robert Gripshover <robertgrips@gmail.com>
2023-03-08 12:52:54 -0500

    finished the post request

--- api/server.js
+++ api/server.js
@@ -12,8 +12,8 @@ server.use(express.json()) //<< gives express the ability to read and parse JSON
 server.post('/api/users', (req, res) => {
     const user = req.body;
     if (!user.name || !user.bio) {
-        res.status(422).json({
-            message: "name and bio required"
+        res.status(400).json({
+            message: "Please provide name and bio for the user"
         })
     } else {
     User.insert(user)
